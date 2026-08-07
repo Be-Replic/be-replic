98b7fb7f97d97a5ca0745a5f6a400644d4b7a0ca
Be-Replic <contact@be-replic.com>
2026-08-07 15:26:05 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 console.log("be-replic");
+
+
+fetch("data/realisations.json")
+.then(response => response.json())
+.then(realisations => {
+
+    const gallery = document.getElementById("gallery-container");
+
+    realisations.forEach(realisation => {
+
+        gallery.innerHTML += `
+
+        <div class="project">
+
+            <img src="images/realisations/${realisation.image}" 
+                 alt="${realisation.titre}">
+
+            <h3>${realisation.titre}</h3>
+
+            <p>${realisation.description}</p>
+
+        </div>
+
+        `;
+
+    });
+
+});
+
+
+
+fetch("data/realisations.json")
+.then(response => {
+    console.log(response);
+    return response.json();
+})
+.then(realisations => {
+
+    console.log(realisations);
+
+});
